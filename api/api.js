3a71ed846e91848e0ba2c6d8c8bc47b8f085809f
Romain <borgromain@gmailcom>
2019-04-30 17:04:58 +0200

    commit exam

--- api/api.js
+++ api/api.js
@@ -3,7 +3,36 @@ const express = require('express');
 const PeopleService = require('./people-service');
 const peopleService = new PeopleService();
 const app = express();
-
-// To be implemented!
+const v1 = express.Router();
 
 module.exports = app;
+
+
+const bodyParser = require('body-parser');
+
+// Basic Auth
+const { digestAuth } = require('../basic-auth/basic-auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api/v1', v1);
+
+app.get('/people', async (request, response) => {
+    const data = await peopleService.getPeople();
+    response.send(data);
+});
+
+app.put('/people/:id', async (request, response) => {
+    const id = request.params.id;
+    const people = {...request.name };
+    try {
+        const result = await peopleService.updatePeople(id, people);
+        response.sendStatus(200);
+    } catch (error) {
+        console.log('error update people: ', error);
+        response.sendStatus(400).end(error);
+    }
+});
+
+
+
--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -1,9 +1,28 @@
 const crypto = require('crypto');
+const hash = crypto.createHash('sha1');
+const hpassword = hash.write('password');
+hash.end();
 
 function sha1Encode(data) {
     // To be implemented!
+    let hash = crypto.createHash('sha1');
+     let response = hash.write(data);
+     hash.end();
+     return response;
 }
 
 module.exports.digestAuth = (request, response, next) => {
     // To be implemented!
+    
+    const authorization = request.headers.authorization;
+    const encoded = authorization.replace('Basic ', '');
+    const decoded = Buffer.from(encoded, 'base64').toString('utf8');
+
+    const [login, password] = decoded.split(':');
+    let pwd = sha1Encode(password);
+    if (login === 'node' && pwd === hpassword) {
+        return next();
+    }else {
+        response.sendStatus(401);
+    }
 }
